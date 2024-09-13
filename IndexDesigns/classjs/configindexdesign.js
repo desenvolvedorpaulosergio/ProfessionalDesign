3cf17ddb29c7a424a4b30a7c5874428ad8b20645
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-09-13 13:19:58 -0300

    Atualizar

---
+++ IndexDesigns/classjs/configindexdesign.js
@@ -0,0 +1,10 @@
+// <!-- 
+        
+// srcdoc=" <br> <br> <br> <br>
+//     <h1 style='text-align:center; font-size: 60px; color: #AFAFAF;'>
+//     Selecione meus Projetos Criativos <br> para Visualizá-los!</h1>
+//     <p style='text-align:center; font-size: 30px; color: #AFAFAF; font-family: arial;'>
+//     Precione a opção de <ins>menu</ins>.</p>
+// "
+
+// -->
