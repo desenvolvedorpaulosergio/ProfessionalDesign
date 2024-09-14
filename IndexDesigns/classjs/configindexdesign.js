22ac096d2d5e6374850ec6c79fcf13dc61a9b450
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-09-14 16:45:58 -0300

    Atualizando configuração JS

--- IndexDesigns/classjs/configindexdesign.js
+++ IndexDesigns/classjs/configindexdesign.js
@@ -1,10 +1,29 @@
 function EventoEnter(){
-    let displayContainer = window.document.getElementById('config-display-container')
-    if(window.document.getElementById('config-iframe').style.src === " "){
+    let displayContainer = window.document.getElementById('config-display-container').value
+    if(window.document.getElementById('config-iframe').style.src === ""){
         displayContainer.style.display = 'block'
     }else{
         //Direcionamento LINK
         alert('Direcionamento LINK')
-        window.location = "https://desenvolvedorpaulosergio.github.io/ProfessionalProjectsHTMLCSSandJS/ProjectsHTMLCSSJS/ThematicSystems/PizzariaRapidista/classhtml/index.html";
+    }
+}
+
+function EventoDownload(){
+    let displayContainer = window.document.getElementById('config-display-container')
+    if(window.document.getElementById('config-iframe').style.src === ""){
+        displayContainer.style.display = 'block'
+    }else{
+        //Download Arquivo
+        alert('Download Arquivo')
+    }
+}
+
+function EventoFullScreen(){
+    let displayContainer = window.document.getElementById('config-display-container')
+    if(window.document.getElementById('config-iframe').style.src === ""){
+        displayContainer.style.display = 'block'
+    }else{
+        //Modo Full-Screen
+        alert('Modo Full-Screen')
     }
 }
