e7b055f20b50218e157f4ceba3800c3a161fa3fd
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-09-14 12:39:35 -0300

    Atuaizar

--- IndexDesigns/classjs/configindexdesign.js
+++ IndexDesigns/classjs/configindexdesign.js
@@ -1,8 +1,10 @@
 function EventoEnter(){
     let displayContainer = window.document.getElementById('config-display-container')
-    if(window.document.getElementById('config-iframe').style.src == ""){
+    if(window.document.getElementById('config-iframe').style.src === " "){
         displayContainer.style.display = 'block'
     }else{
         //Direcionamento LINK
+        alert('Direcionamento LINK')
+
     }
 }
