65a7846a941dddb30ca9b7dac011df925687f1d5
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-09-15 10:12:36 -0300

    Atualizar responsividade

--- IndexDesigns/classjs/configindexdesign.js
+++ IndexDesigns/classjs/configindexdesign.js
@@ -1,29 +1,31 @@
 function EventoEnter(){
-    let displayContainer = window.document.getElementById('config-display-container').value
-    if(window.document.getElementById('config-iframe').style.src === ""){
-        displayContainer.style.display = 'block'
-    }else{
-        //Direcionamento LINK
-        alert('Direcionamento LINK')
-    }
-}
-
-function EventoDownload(){
     let displayContainer = window.document.getElementById('config-display-container')
     if(window.document.getElementById('config-iframe').style.src === ""){
         displayContainer.style.display = 'block'
     }else{
         //Download Arquivo
-        alert('Download Arquivo')
+        alert('Acessando Site....')
     }
 }
 
-function EventoFullScreen(){
-    let displayContainer = window.document.getElementById('config-display-container')
-    if(window.document.getElementById('config-iframe').style.src === ""){
-        displayContainer.style.display = 'block'
-    }else{
-        //Modo Full-Screen
-        alert('Modo Full-Screen')
-    }
-}
+ function EventoDownload(){
+     let displayContainer = window.document.getElementById('config-display-container')
+     if(window.document.getElementById('config-iframe').style.src === ""){
+         displayContainer.style.display = 'block'
+     }else{
+         //Download Arquivo
+         alert('Download Arquivo')
+     }
+ }
+
+ function EventoFullScreen(){
+     let displayContainer = window.document.getElementById('config-display-container')
+     if(window.document.getElementById('config-iframe').style.src === ""){
+         displayContainer.style.display = 'block'
+     }else{
+         //Modo Full-Screen
+         alert('Modo Full-Screen')
+     }
+ }
+
+ 
