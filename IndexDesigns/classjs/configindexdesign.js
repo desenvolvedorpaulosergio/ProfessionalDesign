56447472de8f0c1056ba7ca32614c265b25dffe7
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-09-17 08:48:17 -0300

    Atualizar

--- IndexDesigns/classjs/configindexdesign.js
+++ IndexDesigns/classjs/configindexdesign.js
@@ -1,3 +1,9 @@
+function tamanhoBody(){
+    if(window.innerWidth > 535){
+        window.document.getElementById('config-display-opcoes').style.display = 'none'  
+    }
+}
+
 function eventoEquiv(){
     if(window.document.getElementById('config-display-opcoes').style.display == 'block'){
         window.document.getElementById('config-display-opcoes').style.display = 'none'
